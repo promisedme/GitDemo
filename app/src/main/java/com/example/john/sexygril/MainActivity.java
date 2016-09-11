d3aa44de6ae8e093ee4101d1dd551b5647e94bb8
daren <316697621@qq.com>
2016-09-11 19:58:47 +0800

    butterKnife的代码使用

--- app/src/main/java/com/example/john/sexygril/MainActivity.java
+++ app/src/main/java/com/example/john/sexygril/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.john.sexygril;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.lv)
+    ListView mListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
     }
 }
