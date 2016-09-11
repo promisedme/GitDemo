6655a324be2a22367b883492f23b339e41e305b7
daren <316697621@qq.com>
2016-09-11 13:31:54 +0800

    11112

--- app/src/main/java/com/example/john/sexygril/MainActivity.java
+++ app/src/main/java/com/example/john/sexygril/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);//123478
+        setContentView(R.layout.activity_main);//1234787
     }
 }
